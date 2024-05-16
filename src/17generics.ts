89d11bae459b7c01eba51751e454ad03befb9db8
FaisalKhan2000 <iamfaisal.luv@gmail.com>
2024-05-16 14:22:34 +0530

    generics - more

--- src/17generics.ts
+++ src/17generics.ts
@@ -49,11 +49,48 @@ const genericString: GenericInterface<string> = {
 
 // A Promise in JavaScript (and thus TypeScript) is an object representing the eventual completion or failure of an asynchronous operation.
 
-async function someFunc(): Promise<string> {
-  return "Hello World";
+// async function someFunc(): Promise<string> {
+//   return "Hello World";
+// }
+
+async function someFunc(): Promise<number> {
+  return 8080;
 }
 
 const promiseResult = someFunc();
 console.log(promiseResult);
 
 // * Generics - Generate Array
+function generateStringArray(length: number, value: string) {
+  let result: string[] = [];
+  result = Array(length).fill(value);
+  return result;
+}
+console.log(generateStringArray(10, "hello"));
+
+function createArray<T>(length: number, value: T): Array<T> {
+  let result: T[] = [];
+  result = Array(length).fill(value);
+  return result;
+}
+
+let arrayStrings = createArray(10, "hello");
+let arrayNumbers = createArray(10, 8080);
+let arrayBooleans = createArray(10, true);
+
+console.log(arrayStrings);
+console.log(arrayNumbers);
+console.log(arrayBooleans);
+
+// * Generics -  Multiple Params
+
+function pair<T, U>(param1: T, param2: U): [T, U] {
+  return [param1, param2];
+}
+
+// Usage
+let pairResult = pair<number, string>(123, "hello");
+
+console.log(pairResult); // Output: [123, "Hello"]
+
+// * Generics - Inferred Type and Type Constraints
